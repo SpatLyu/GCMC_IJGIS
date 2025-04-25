63cb43372026176e08989789482917093c4f55e6
SpatLyu <lyu.geosocial@gmail.com>
2025-04-25 19:35:34 +0800

    finish case3

---
+++ Case of net primary productivity study/Case of net primary productivity study.r
@@ -0,0 +1,122 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~        Case: Farmland NPP In China       ~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~    Author: Wenbo Lv; Date: 2025-04-25    ~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+library(spEDM)
+npp = terra::rast('./Case of net primary productivity study/npp.tif')
+# To save the computation time, we will aggregate the data by 3 times and 
+# select 1500 non-NA pixels to predict:
+npp = terra::aggregate(npp, fact = 3, na.rm = TRUE)
+npp = npp[[c("npp","pre","tem")]]
+npp
+
+terra::global(npp,"isNA")
+terra::ncell(npp)
+
+nnamat = terra::as.matrix(npp[[1]], wide = TRUE)
+nnaindice = which(!is.na(nnamat), arr.ind = TRUE)
+dim(nnaindice)
+
+set.seed(2025)
+indices = sample(nrow(nnaindice), size = 1500, replace = FALSE)
+libindice = nnaindice[-indices,]
+predindice = nnaindice[indices,]
+
+#------------------------------------------------------------------------------#
+#------    Causality by Geographical Cross Mapping Cardinality (GCMC)    ------#
+#------------------------------------------------------------------------------#
+
+# precipitation and npp
+g1 = gcmc(data = npp,cause = "pre",effect = "npp",E = 2,k = 270,
+          lib = predindice, pred = predindice, trend.rm = TRUE)
+g1
+g1$xmap
+
+# temperature and npp
+g2 = gcmc(data = npp,cause = "tem",effect = "npp",E = 2,k = 270,
+          lib = predindice, pred = predindice, trend.rm = TRUE)
+g2
+
+# precipitation and temperature
+g3 = gcmc(data = npp,cause = "pre",effect = "tem",E = 2,k = 270,
+          lib = predindice, pred = predindice, trend.rm = TRUE)
+g3
+
+gcmc_case3 = list(g1,g2,g3)
+readr::write_rds(gcmc_case3,'./Case of net primary productivity study/gcmc_case3.rds')
+
+#------------------------------------------------------------------------------#
+#------    Causality by Geographical Convergent Cross Mapping (GCCM)     ------#
+#------------------------------------------------------------------------------#
+
+# precipitation and npp
+g1 = gccm(data = npp,cause = "pre",effect = "npp",
+          libsizes = as.matrix(expand.grid(seq(10,130,10),seq(10,160,10))),
+          E = 2, k = 6, lib = predindice, pred = predindice)
+g1
+
+# temperature and npp
+g2 = gccm(data = npp,cause = "tem",effect = "npp",
+          libsizes = as.matrix(expand.grid(seq(10,130,10),seq(10,160,10))),
+          E = 2, k = 6, lib = predindice, pred = predindice)
+g2
+
+# precipitation and temperature
+g3 = gccm(data = npp,cause = "pre",effect = "tem",
+          libsizes = as.matrix(expand.grid(seq(10,130,10),seq(10,160,10))),
+          E = 2, k = 6, lib = predindice, pred = predindice)
+g3
+
+gccm_case3 = list(g1,g2,g3)
+readr::write_rds(gccm_case3,'./Case of net primary productivity study/gccm_case3.rds')
+
+#------------------------------------------------------------------------------#
+#------        Correlation by Pearson Correlation Coefficient(PCC)       ------#
+#------------------------------------------------------------------------------#
+
+npp.df = dplyr::filter(npp[terra::cellFromRowCol(npp,predindice[,1],predindice[,2])],
+                       dplyr::if_all(dplyr::everything(),
+                                     \(.x) !is.na(.x)))
+pcc = psych::corr.test(npp.df)
+pcc
+readr::write_rds(pcc,'./Case of net primary productivity study/pcc_case3.rds')
+
+#------------------------------------------------------------------------------#
+#------             Association by Geographical Detector(GD)             ------#
+#------------------------------------------------------------------------------#
+
+source('./Utils/ssh_q.r')
+
+q1 = ssh_q(data = npp.df,cause = "pre",effect = "npp")
+q2 = ssh_q(data = npp.df,cause = "tem",effect = "npp")
+q3 = ssh_q(data = npp.df,cause = "pre",effect = "tem")
+qv = do.call(rbind,list(q1,q2,q3))
+qv
+readr::write_rds(qv,'./Case of net primary productivity study/gd_case3.rds')
+
+#------------------------------------------------------------------------------#
+#------                   Handling the case results                      ------#
+#------------------------------------------------------------------------------#
+
+source('./Utils/process_results.r')
+
+case3 = list(
+  gcmc = readr::read_rds("./Case of net primary productivity study/gcmc_case3.rds") |> 
+    purrr::map(.process_xmap_result) |> 
+    purrr::list_rbind(),
+  gccm = readr::read_rds("./Case of net primary productivity study/gccm_case3.rds") |> 
+    purrr::map(.process_xmap_result,gcmc = FALSE) |> 
+    purrr::list_rbind(),
+  pcc = readr::read_rds("./Case of net primary productivity study/pcc_case3.rds") |>
+    .process_pcc_result(),
+  gd = readr::read_rds("./Case of net primary productivity study/gd_case3.rds") |>
+    dplyr::select(cause = x, effect = y, ca = qv, sig)
+)
+case3
+
+writexl::write_xlsx(case3,"./Case of net primary productivity study/Case of net primary productivity study.xlsx")
