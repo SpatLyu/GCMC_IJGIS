4cc1625bbb65085543bb3f3d3c42ff9ecd3f5b1a
SpatLyu <lyu.geosocial@gmail.com>
2025-12-15 11:24:35 +0800

    update case study 1

--- Case of residential crime study/Case of residential crime study.r
+++ Case of residential crime study/Case of residential crime study.r
@@ -10,22 +10,28 @@ library(spEDM)
 columbus = sf::read_sf("./Case of residential crime study/columbus.gpkg")
 columbus
 
+#-----------------------------------------------------------------------------#
+#------            Determining minimum embedding dimension              ------#
+#-----------------------------------------------------------------------------#
+
+spEDM::fnn(columbus, "hoval", E = 1:10, eps = stats::sd(columbus$hoval) / 10)
+
 #------------------------------------------------------------------------------#
 #------    Causation by Geographical Cross Mapping Cardinality (GCMC)    ------#
 #------------------------------------------------------------------------------#
 
-fnn(columbus, "crime", E = 1:15)
+ceiling(sqrt(7 * nrow(columbus)))
 
 # housing value and crime (residential burglaries and vehicle thefts)
-g1 = gcmc(columbus,"hoval","crime", E = 7, k = 25)
+g1 = gcmc(columbus, "hoval", "crime", E = 7, k = 19)
 g1
 
 # household income and crime (residential burglaries and vehicle thefts)
-g2 = gcmc(columbus,"inc","crime", E = 7, k = 25)
+g2 = gcmc(columbus, "inc", "crime", E = 7, k = 19)
 g2
 
 # housing value and household income
-g3 = gcmc(columbus,"hoval","inc", E = 7, k = 25)
+g3 = gcmc(columbus, "hoval", "inc", E = 7, k = 19)
 g3
 
 gcmc_case1 = list(g1,g2,g3)
