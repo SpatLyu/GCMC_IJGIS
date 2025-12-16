a4f3d1af8990978cfec6b7b24f0f527d154525e3
SpatLyu <lyu.geosocial@gmail.com>
2025-12-16 22:03:27 +0800

    rearrange figures

--- Case of net primary productivity study/Figure7.r
+++ Case of net primary productivity study/Figure7.r
@@ -1,7 +1,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-#~~~~~~~~~~~~~~~~~~~                  Figure 6                ~~~~~~~~~~~~~~~~#
+#~~~~~~~~~~~~~~~~~~~                  Figure 7                ~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -9,56 +9,56 @@
 source('./Utils/plot_cs_matrix.r')
 
 #------------------------------------------------------------------------------#
-#---------------------------      Figure 6a       -----------------------------#
+#---------------------------      Figure 7a       -----------------------------#
 #------------------------------------------------------------------------------#
 
 pcc = readxl::read_xlsx("./Case of net primary productivity study/Case of net primary productivity study.xlsx",
                         sheet = "pcc")
-fig6a = plot_cs_matrix(pcc)
-fig6a
-ggplot2::ggsave('./Case of net primary productivity study/fig6a.png',
-                fig6a, width = 3.65, height = 4.05, dpi = 300)
+fig7a = plot_cs_matrix(pcc)
+fig7a
+ggplot2::ggsave('./Case of net primary productivity study/fig7a.png',
+                fig7a, width = 3.65, height = 4.05, dpi = 300)
 
 #------------------------------------------------------------------------------#
-#---------------------------      Figure 6b       -----------------------------#
+#---------------------------      Figure 7b       -----------------------------#
 #------------------------------------------------------------------------------#
 
 gd = readxl::read_xlsx("./Case of net primary productivity study/Case of net primary productivity study.xlsx",
                        sheet = "gd")
-fig6b = plot_cs_matrix(gd)
-fig6b
-ggplot2::ggsave('./Case of net primary productivity study/fig6b.png',
-                fig6b, width = 3.65, height = 4.05, dpi = 300)
+fig7b = plot_cs_matrix(gd)
+fig7b
+ggplot2::ggsave('./Case of net primary productivity study/fig7b.png',
+                fig7b, width = 3.65, height = 4.05, dpi = 300)
 
 #------------------------------------------------------------------------------#
-#---------------------------      Figure 6b       -----------------------------#
+#---------------------------      Figure 7c       -----------------------------#
 #------------------------------------------------------------------------------#
 
 directlingam = readxl::read_xlsx("./Case of net primary productivity study/Case of net primary productivity study.xlsx",
                                  sheet = "directlingam")
-fig6c = plot_cs_matrix(directlingam)
-fig6c
-ggplot2::ggsave('./Case of net primary productivity study/fig6c.png',
-                fig6c, width = 3.65, height = 4.05, dpi = 300)
+fig7c = plot_cs_matrix(directlingam)
+fig7c
+ggplot2::ggsave('./Case of net primary productivity study/fig7c.png',
+                fig7c, width = 3.65, height = 4.05, dpi = 300)
 
 #------------------------------------------------------------------------------#
-#---------------------------      Figure 6d       -----------------------------#
+#---------------------------      Figure 7d       -----------------------------#
 #------------------------------------------------------------------------------#
 
 gccm = readxl::read_xlsx("./Case of net primary productivity study/Case of net primary productivity study.xlsx",
                          sheet = "gccm")
-fig6d = plot_cs_matrix(gccm)
-fig6d
-ggplot2::ggsave('./Case of net primary productivity study/fig6d.png',
-                fig6d, width = 3.65, height = 4.05, dpi = 300)
+fig7d = plot_cs_matrix(gccm)
+fig7d
+ggplot2::ggsave('./Case of net primary productivity study/fig7d.png',
+                fig7d, width = 3.65, height = 4.05, dpi = 300)
 
 #------------------------------------------------------------------------------#
-#---------------------------      Figure 6e       -----------------------------#
+#---------------------------      Figure 7e       -----------------------------#
 #------------------------------------------------------------------------------#
 
 gcmc = readxl::read_xlsx("./Case of net primary productivity study/Case of net primary productivity study.xlsx",
                          sheet = "gcmc")
-fig6e = plot_cs_matrix(gcmc)
-fig6e
-ggplot2::ggsave('./Case of net primary productivity study/fig6e.png',
-                fig6e, width = 3.65, height = 4.05, dpi = 300)
+fig7e = plot_cs_matrix(gcmc)
+fig7e
+ggplot2::ggsave('./Case of net primary productivity study/fig7e.png',
+                fig7e, width = 3.65, height = 4.05, dpi = 300)
