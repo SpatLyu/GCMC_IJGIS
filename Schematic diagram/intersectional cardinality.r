a77cca0d9c74ec53b7c71bea3bc839a050a84cac
SpatLyu <lyu.geosocial@gmail.com>
2025-12-16 18:16:00 +0800

    update ic symbols

--- Schematic diagram/intersectional cardinality.r
+++ Schematic diagram/intersectional cardinality.r
@@ -64,13 +64,13 @@ plot3D::lines3D(My[,1], My[,2], My[,3],
                 colvar = NULL, pch = 19, col = "grey70",
                 theta = 10, phi = 0, lwd = 0.15, bty = "n")
 
-plot3D::scatter3D(Mx[NNx,1], Mx[NNx,2], Mx[NNx,3],
-                colvar = NULL, pch = 19, col = "#627b9cff",
-                theta = 10, phi = 0, cex = 0.15, bty = "n", add = TRUE)
+plot3D::scatter3D(My[NNx,1], My[NNx,2], My[NNx,3],
+                  colvar = NULL, pch = 19, col = "#627b9cff",
+                  theta = 10, phi = 0, cex = 0.15, bty = "n", add = TRUE)
 
 plot3D::scatter3D(My[1,1], My[1,2], My[1,3],
-                colvar = NULL, pch = 19, col = "#055ad0ff",
-                theta = 10, phi = 0, cex = 0.55, bty = "n", add = TRUE)
+                  colvar = NULL, pch = 19, col = "#055ad0ff",
+                  theta = 10, phi = 0, cex = 0.55, bty = "n", add = TRUE)
 
 dev.off()
 
@@ -105,9 +105,9 @@ plot3D::lines3D(My[,1], My[,2], My[,3],
                 colvar = NULL, pch = 19, col = "grey70",
                 theta = 10, phi = 0, lwd = 0.15, bty = "n")
 
-plot3D::scatter3D(Mx[NNy,1], Mx[NNy,2], Mx[NNy,3],
+plot3D::scatter3D(My[NNy,1], My[NNy,2], My[NNy,3],
                   colvar = NULL, pch = 19, col = "#627b9cff",
-                  theta = 10, phi = 0, cex = 0.15, bty = "n", add = TRUE)
+                  theta = 10, phi = 0, cex = 0.05, bty = "n", add = TRUE)
 
 plot3D::scatter3D(My[1,1], My[1,2], My[1,3],
                   colvar = NULL, pch = 19, col = "#055ad0ff",
@@ -124,7 +124,7 @@ plot3D::lines3D(My[,1], My[,2], My[,3],
                 colvar = NULL, pch = 19, col = "grey70",
                 theta = 10, phi = 0, lwd = 0.35, bty = "n")
 
-plot3D::scatter3D(Mx[NNy,1], Mx[NNy,2], Mx[NNy,3],
+plot3D::scatter3D(My[NNy,1], My[NNy,2], My[NNy,3],
                   xlim = NN_range,
                   ylim = NN_range,
                   zlim = NN_range,
